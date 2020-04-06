24d72118494d9943766bbe338c667226158a25dc
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-04-06 21:23:16 +0100

    bunch of updates to tidy things up

--- munch/src/main.cpp
+++ munch/src/main.cpp
@@ -1,9 +1,15 @@
 #include "noms/fruit/apple.h"
+#include "noms/veg/asparugus.h"
+
 #include <iostream>
 
 int main()
 {
     noms::Apple apple("red");
     std::cout << "The apple is " << apple.getColour() << std::endl;
+
+    noms::Aspargus aspargus{};
+    //std::cout << "The asparagus is " << asparagus.() << std::endl;
+
     return 0;
 }
--- noms/veg/include/noms/veg/asparugus.h
+++ noms/veg/include/noms/veg/asparugus.h
@@ -1,9 +1,11 @@
 #ifndef _ASPARAGUS_H
 #define _ASPARAGUS_H
 
-class Asparagus
+namespace noms
 {
-
+class Aspargus
+{
 };
+}
 
 #endif /* _ASPARAGUS_H */
