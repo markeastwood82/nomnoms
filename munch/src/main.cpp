f81fbb88a0b8d2fb1f0bd32ddef2b31afb70a835
ClausKlein <claus.klein@arcormail.de>
2021-03-08 15:12:59 +0100

    change to the way the cmake guide use components

--- munch/src/main.cpp
+++ munch/src/main.cpp
@@ -1,9 +1,14 @@
 #include <noms/fruit/apple.h>
+#include <noms/veg/asparagus.h>
 #include <iostream>
 
 int main()
 {
     noms::Apple apple("red");
     std::cout << "The apple is " << apple.getColour() << std::endl;
+
+    noms::Asparagus asparagus("green");
+    std::cout << "The asparagus is " << asparagus.getColour() << std::endl;
+
     return 0;
 }
